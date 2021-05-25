aceae1f415272519092c8ae18f418d5bb56785d5
Subhayan Bhattacharya <subhayan.bhattacharya@ai4bd.com>
2021-05-25 17:00:24 +0200

    Use marks instead of classes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 from application import app
 from application import mongo
@@ -63,56 +64,59 @@ def _check_book_correctness_book_1(response_book):
     }
 
 
-class TestGettingSingleBook:
-    @staticmethod
-    def test_get_single_book_by_id(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.get("/book/book_1")
-        assert response.status_code == 200
-        jsonified_response = response.json()
-        _check_book_correctness_book_1(jsonified_response)
-
-    @staticmethod
-    def test_get_single_book_if_it_does_not_exist(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.get("/book/book_100")
-        assert response.status_code == 400
-        assert response.json() == {"message": "No such book exist!!"}
-
-    @staticmethod
-    def test_get_single_book_with_correct_etag(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.get("/book/book_1", headers={"If-None-Match": "book_1"})
-        assert response.status_code == 304
-
-    @staticmethod
-    def test_get_single_book_with_wrong_etag(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.get("/book/book_1", headers={"If-None-Match": "book_10"})
-        assert response.status_code == 200
-        jsonified_response = response.json()
-        _check_book_correctness_book_1(jsonified_response)
+@pytest.mark.get_single_book
+def test_get_single_book_by_id(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.get("/book/book_1")
+    assert response.status_code == 200
+    jsonified_response = response.json()
+    _check_book_correctness_book_1(jsonified_response)
 
 
-class TestDeleteSingleBook:
-    @staticmethod
-    def test_delete_book_if_not_exists(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.delete("/book/book_100")
-        assert response.status_code == 400
-        assert response.json() == {"message": "No such book exist!!"}
+@pytest.mark.get_single_book
+def test_get_single_book_if_it_does_not_exist(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.get("/book/book_100")
+    assert response.status_code == 400
+    assert response.json() == {"message": "No such book exist!!"}
 
-    @staticmethod
-    def test_delete_book_with_correct_etag(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.delete("/book/book_1", headers={"If-Match": "book_1"})
-        assert response.status_code == 200
-        assert response.json() == {"message": "Book deleted !!"}
 
-    @staticmethod
-    def test_delete_book_with_wrong_etag(backend):
-        mongo.BACKEND = backend(books=all_books)
-        response = client.delete("/book/book_1", headers={"If-Match": "book_100"})
-        assert response.status_code == 412
+@pytest.mark.get_single_book
+def test_get_single_book_with_correct_etag(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.get("/book/book_1", headers={"If-None-Match": "book_1"})
+    assert response.status_code == 304
+
+
+@pytest.mark.get_single_book
+def test_get_single_book_with_wrong_etag(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.get("/book/book_1", headers={"If-None-Match": "book_10"})
+    assert response.status_code == 200
+    jsonified_response = response.json()
+    _check_book_correctness_book_1(jsonified_response)
+
+
+@pytest.mark.delete_single_book
+def test_delete_book_if_not_exists(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.delete("/book/book_100")
+    assert response.status_code == 400
+    assert response.json() == {"message": "No such book exist!!"}
+
+
+@pytest.mark.delete_single_book
+def test_delete_book_with_correct_etag(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.delete("/book/book_1", headers={"If-Match": "book_1"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Book deleted !!"}
+
+
+@pytest.mark.delete_single_book
+def test_delete_book_with_wrong_etag(backend):
+    mongo.BACKEND = backend(books=all_books)
+    response = client.delete("/book/book_1", headers={"If-Match": "book_100"})
+    assert response.status_code == 412
 
 
