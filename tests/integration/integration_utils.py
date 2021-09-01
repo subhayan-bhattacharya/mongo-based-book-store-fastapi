61e67c12c62917db8a801d0aeda47a41689240ec
Subhayan <subhayan.aws@gmail.com>
2021-09-01 23:36:35 +0200

    adding more tavern tests

--- tests/integration/integration_utils.py
+++ tests/integration/integration_utils.py
@@ -2,11 +2,11 @@ import string
 import json
 
 
-def validate_new_book_response(response, inserted_book):
+def validate_create_update_book_response(response, inserted_book):
     response_data = response.json()
     replaced_inserted_data = inserted_book.replace("\'", "\"")
     inserted_data = json.loads(replaced_inserted_data)
-    for key in inserted_book:
+    for key in inserted_data:
         if key == "author":
             assert response_data.get("author") == string.capwords(inserted_data.get("author"))
         else:
