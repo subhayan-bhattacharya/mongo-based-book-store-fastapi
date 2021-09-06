17ca2e83a2ce7a370bad886e85cb84f2a243a7b7
Subhayan Bhattacharya <subhayan.bhattacharya@elevait.de>
2021-09-06 10:40:04 +0200

    use yaml anchors and refactor functions

--- tests/integration/integration_utils.py
+++ tests/integration/integration_utils.py
@@ -30,13 +30,23 @@ def updated_book_data_to_insert():
     return Box(updated_book_data())
 
 
-def validate_book_response(response, inserted_book):
-    response_data = response.json()
-    replaced_inserted_data = inserted_book.replace("\'", "\"")
-    inserted_data = json.loads(replaced_inserted_data)
-    for key in inserted_data:
+def _validate_inserted_with_retrieved(inserted, response):
+    for key in inserted:
         if key == "author":
-            assert response_data.get("author") == string.capwords(inserted_data.get("author"))
+            assert response.get("author") == string.capwords(inserted.get("author"))
         else:
-            assert response_data.get(key) == inserted_data.get(key)
+            assert response.get(key) == inserted.get(key)
+
+
+def validate_book_response(response, book_data_with_request=None, inserted_book_data=None):
+    response_data = response.json()
+    inserted_data = None
+    if book_data_with_request is not None:
+        replaced_inserted_data = book_data_with_request.replace("\'", "\"")
+        inserted_data = json.loads(replaced_inserted_data)
+    if inserted_book_data is not None:
+        inserted_data = inserted_book_data
+    assert inserted_data is not None
+    _validate_inserted_with_retrieved(inserted=inserted_data, response=response_data)
     assert "link" in response_data
+
